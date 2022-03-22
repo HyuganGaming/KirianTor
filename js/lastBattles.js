1487189fd37e987b407e39d1e16e6a03ce37dbfe
HyuganGaming <101477898+HyuganGaming@users.noreply.github.com>
2022-03-22 16:47:10 -0300

    pagination update

--- js/lastBattles.js
+++ js/lastBattles.js
@@ -1,9 +1,23 @@
-var kirian_tor_battles_array = [];
+let kirian_tor_battles_array = [];
+let amountOfBattlesPerPage = 15;
+let currentPage = 1;
+let start;
+let end;
 
-function ShowLastBattles(array) {
+function ShowLastBattles(page) {
     let content = "";
+    let array = kirian_tor_battles_array;
 
-    for (let i = 1; i < array.length; i++) {
+    currentPage = page;
+    AddPagination(kirian_tor_battles_array.length - 1);
+
+    start = array.length-1 - ((page - 1) * amountOfBattlesPerPage);
+    end = array.length-1 - (page * amountOfBattlesPerPage)
+    if (end < 0){
+        end = 0;
+    }
+
+    for (let i = start; i > end; i--) {
 
         let battle = array[i];
 
@@ -43,12 +57,68 @@ function ShowLastBattles(array) {
     document.getElementById("tableBattles").innerHTML = content;
 }
 
+function AddPagination(amountOfBattles){
+    let content = "";
+    let amountOfPages = Number(Math.ceil(amountOfBattles / amountOfBattlesPerPage));
+
+    let previousPage = currentPage - 1;
+    let nextPage = currentPage + 1;
+    if (previousPage < 1){
+        previousPage = 1;
+    }
+    if (nextPage > amountOfPages){
+        nextPage = amountOfPages;
+    }
+
+    if (amountOfBattles < amountOfBattlesPerPage){
+
+    }
+    else {
+        content +=
+        `
+        <li class="page-item">
+            <button class="page-link" onclick="ShowLastBattles(${previousPage})">Previous</button>
+        </li>
+        `
+
+        for (let i = 0; i < amountOfPages; i++){
+
+            if (i+1 == currentPage){
+                content +=
+                `
+                <li class="page-item active">
+                    <button class="page-link" onclick="ShowLastBattles(${i+1})">${i+1}</button>
+                </li>
+                `
+            }
+            else{
+                content +=
+                `
+                <li class="page-item">
+                    <button class="page-link" onclick="ShowLastBattles(${i+1})">${i+1}</button>
+                </li>
+                `
+            }
+        }
+
+        content +=
+        `
+        <li class="page-item">
+            <button class="page-link" onclick="ShowLastBattles(${nextPage})">Next</button>
+        </li>
+        `
+
+        document.getElementById("pagination").innerHTML = content;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(KIRIAN_TOR_HOUSES_BATTLES).then(function (resultObj) {
         if (resultObj.status === "ok") {
             kirian_tor_battles_array = resultObj.data.values;
 
-            ShowLastBattles(kirian_tor_battles_array);
+            AddPagination(kirian_tor_battles_array.length - 1);
+            ShowLastBattles(currentPage);
         }
     });
 });
