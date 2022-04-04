49ebb3d3023f8354c2feea99f7a62fd36c446e6b
HyuganGaming <101477898+HyuganGaming@users.noreply.github.com>
2022-04-04 03:58:03 -0300

    fix nav

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
     `
     <nav class="navbar navbar-expand-lg navbar-dark bg-darker">
         <div class="container">
-            <a class="navbar-brand" href="index.html">Houses Ranking</a>
+            <a class="navbar-brand" href="/index.html">Houses Ranking</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
             <div class="collapse navbar-collapse row" id="navbarNavAltMarkup">
                 <div class="navbar-nav col">
-                    <a class="nav-link col text-center" aria-current="page" href="rankingPlayers.html">Players Ranking</a>
-                    <a class="nav-link col text-center" aria-current="page" href="lastBattles.html">Last Battles</a>
-                    <a class="nav-link col text-center" aria-current="page" href="result.html">Upload result</a>
+                    <a class="nav-link col text-center" aria-current="page" href="/rankingPlayers.html">Players Ranking</a>
+                    <a class="nav-link col text-center" aria-current="page" href="/lastBattles.html">Last Battles</a>
+                    <a class="nav-link col text-center" aria-current="page" href="/result.html">Upload result</a>
 
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
